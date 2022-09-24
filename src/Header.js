126a6c2618824b2369d4cc8c3459a24517bdaa71
Anisa <anissa.sahiiti@gmail.com>
2022-09-24 20:27:50 +0200

    ADD: Header

---
+++ src/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import "./Header.css";
+
+
+function Header() {
+
+    return (
+
+        <div className="header">
+            <a href="#defalut" className="logo">Meroje</a>
+
+            <div className="header__right">
+                <a href="#default">Welcome</a>
+                <a href="#default">Home</a>
+                <a href="#default">About Us</a>
+                <a href="#default">Contact</a>
+                <a href="#default">Photo</a>
+                <a href="#default">Video</a>
+                <a href="#default">Drone</a>
+            </div>
+
+
+
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Nav.css";
+
+function Nav() {
+    return (
+        <div className="nav">
+            <h4>Video Produciton</h4>
+        </div>
+    )
+}
+
+export default Nav;
