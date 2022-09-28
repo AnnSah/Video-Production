11ee7b9104afba3b6087f5512ed7fb8bdcb5f8bd
Anisa <anissa.sahiiti@gmail.com>
2022-09-29 01:03:43 +0200

    ADD: added images

--- src/Photo/LandScape/LandLayout.js
+++ src/Photo/LandScape/LandLayout.js
@@ -5,13 +5,13 @@ function LandLayout() {
     return (
         <div className="land__layout">
             <img className="land-grid-col-1 land-grid-row-2" src="./sub-images/lake.jpg" />
-            <img src="./sub-images/lifestyle1.jpg" />
-            <img src="./sub-images/lifestyle1.jpg" />
+            <img src="./sub-images/ice.jpg" />
+            <img src="./sub-images/hill.jpg" />
             <img className="land-grid-col-1 land-grid-row-2" src="./sub-images/paris.jpg" />
-            <img src="./sub-images/lifestyle1.jpg" />
+            <img src="./sub-images/fall.jpg" />
             <img className="land-grid-col-1 land-grid-row-2" src="./sub-images/ballon.jpg" />
-            <img src="./sub-images/lifestyle1.jpg" />
-            <img className="land-grid-col-1 land-grid-row-2" src="./sub-images/lifestyle1.jpg" />
+            <img src="./sub-images/mountain.jpg" />
+            <img className="land-grid-col-1 land-grid-row-2" src="./sub-images/air.jpg" />
 
         </div>
     )
--- src/Photo/LandScape/LandScape.js
+++ src/Photo/LandScape/LandScape.js
@@ -11,7 +11,7 @@ function LandScape() {
                     <h3>Nature Photographer</h3>
                     <p>Nature photography is a genre of photography that unlocks the beauty of natural elements in their natural habitat within a frame. It is an umbrella term that covers landscape photography, wildlife photography, and garden photography.</p>
                     <h2>Contact Us & let's talk ideas!</h2>
-
+                    <button>Contact Us</button>
                 </div>
             </div>
             <LandLayout />
