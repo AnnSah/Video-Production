29bf5b7384bcaacc80948ec564db0685b950cc22
Anisa <anissa.sahiiti@gmail.com>
2022-10-01 16:34:57 +0200

    ADD: videos and media queries

--- src/Video/Video.js
+++ src/Video/Video.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import "./Video.css";
 import ReactPlayer from "react-player";
+import GridVideo from './GridVideo';
 
 function Video() {
     return (
         <div className="video__player">
-            <ReactPlayer className="video__1" url="https://www.youtube.com/watch?v=tCpiZ7RQ98M&t=7s" />
-            <h1>Video Production</h1>
-            <p>Video production is basically the entire process of creating a video. Whether it's a short film, a full-length movie, business marketing video, television commercial, music video, or other type of film, the process may vary a little with the specifics, but the overall process is basically the same.</p>
+            <div className="video__description">
+                <ReactPlayer className="video__1" width="100%" url="https://youtu.be/9ZfN87gSjvI" />
+                <h1>Audiovisual Production</h1>
+            </div>
+
+
+            <GridVideo />
         </div>
+
+
     )
 }
 
