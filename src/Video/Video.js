324784c80edc16e7f2e6ae7a7bf8688f95da0f0b
Anisa <anissa.sahiiti@gmail.com>
2022-10-02 18:28:04 +0200

    ADD: added fonts

--- src/Video/Video.js
+++ src/Video/Video.js
@@ -9,12 +9,18 @@ function Video() {
             <div className="video__description">
                 <ReactPlayer className="video__1" width="100%" url="https://youtu.be/9ZfN87gSjvI" />
                 <h1>Audiovisual Production</h1>
+
+                <div className="video__description_2">
+                    <h2>Our Video Production</h2>
+                    <p>Our audiovisual production agency offers you personalized rates adapted to your budget and your objectives. Produce a quality, creative video that highlights your service, product or company. The goal is to convey your message to your target audience.</p>
+                    <p>Our audiovisual production agency offers you personalized rates adapted to your budget and your objectives. Produce a quality, creative video that highlights your service, product or company. The goal is to convey your message to your target audience.</p>
+                </div>
             </div>
 
             <GridVideo />
-            <div className="video__description_2">
-                <h2>Our Video Production</h2>
-            </div>
+          <div className="video__info">
+            
+          </div>
         </div>
 
 
