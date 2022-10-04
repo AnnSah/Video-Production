7045c28ee888e03aee3d795b04d38bfc3e0e66c4
Anisa <anissa.sahiiti@gmail.com>
2022-10-04 21:22:10 +0200

    ADD: added reactplayer

--- src/Video/VideoPackage/DroneVideo.js
+++ src/Video/VideoPackage/DroneVideo.js
@@ -8,6 +8,15 @@ function DroneVideo() {
             <div className="drone__video">
                 <ReactPlayer width="25%" url="https://youtu.be/Yo4ueevKfdE" />
                 <ReactPlayer width="25%" url="https://youtu.be/FCPdIvXo2rU" />
+                <ReactPlayer width="25%" url="https://youtu.be/8Z1eMy2FoX4" />
+            </div>
+
+            <div className="drone__second">
+                <ReactPlayer width="25%" url="https://youtu.be/6k7a8bw451M" />
+                <ReactPlayer width="25%" url="https://youtu.be/xRFrjAVH1uQ" />
+                <ReactPlayer width="25%" url="https://youtu.be/xRFrjAVH1uQ" />
+
+
             </div>
         </div>
     )
