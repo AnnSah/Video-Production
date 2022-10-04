995f3dbe6c0d04ef80cb9ceaee4924464374a21f
Anisa <anissa.sahiiti@gmail.com>
2022-10-04 14:49:56 +0200

    ADD: added elements

--- src/Video/VideoPackage/VideoPackage.js
+++ src/Video/VideoPackage/VideoPackage.js
@@ -8,13 +8,29 @@ function VideoPackage() {
             <div className="package__title">
                 <h2>Video Editing, Granding, fx, montage</h2>
             </div>
+
             <div className="package__description">
-                <img src="./images2/editing.jpg" />
+                <img src="./images2/editing.jpg" alt="man editing" />
                 <div className="package__text">
                     <h3>Video Editing</h3>
                     <p>Video editing is one of the most important steps in your audio visual production project. This is why our video editing experts bring you all their expertise in this area. A beautiful video capture combined with a controlled editing will offer you a quality production.</p>
                 </div>
             </div>
+
+            <div className="package__granding">
+                <img src="./images2/color.jpg" alt="color editing" />
+                <div className="package__granding_text">
+                    <h3>Video & Color Granding</h3>
+                    <p>Color grading is an important step in the filmmaking process. Yet many underestimate it. First, the retouching of colors and brightness improves the rendering of the video . Then, the color grading gives a particular atmosphere to the videos. Finally, video dressing makes it possible to embed titles, subtitles or graphics  in a film. Therefore, a handy step when wanting to provide additional information in a video.</p>
+                </div>
+            </div>
+
+            <div className="package__details">
+                <h1>Content creation, a winning strategy</h1>
+                <p>With the impact of video on the web, multimedia content guarantees positive results in your advertising campaigns. They allow you to have a better impact than simple photos. It is important that video production  is at the heart of your strategy.
+                    80% of online userswill watch a video, while only 20% will read content on the site.</p>
+            </div>
+
         </div>
     )
 }
