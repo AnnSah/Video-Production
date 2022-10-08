1cc8518775ad1a2108f33034546530faf0924446
Anisa S <96669684+AnnSah@users.noreply.github.com>
2022-10-08 18:05:16 +0200

    fixed the paragraph

--- src/Home/Nature.js
+++ src/Home/Nature.js
@@ -9,7 +9,8 @@ function Nature() {
             <div className="nature__details">
                 <h2>Paysage Photography</h2>
                 <h4>Landcapes, Aerial View</h4>
-                <p>Landscape photography shows the spaces within the world, sometimes vast and unending, but other times microscopic. Landscape photographs typically capture the presence of nature but can also focus on man-made features or disturbances of landscapes.</p>
+                <p>Landscape photography shows the spaces within the world, sometimes vast and unending, but other times microscopic.
+                   Landscape photographs typically capture the presence of nature but can also focus on man-made features or disturbances of landscapes.</p>
                 <Link className="nature__details__btn" to="/landscape">
                     <button>Learn More</button>
                 </Link>
