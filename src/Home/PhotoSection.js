e3b6690e744be373caf9e8d979464804aef6bce2
Anisa <anissa.sahiiti@gmail.com>
2022-10-08 18:00:07 +0200

    ADd: changed the paragraph width.

--- src/Home/PhotoSection.js
+++ src/Home/PhotoSection.js
@@ -8,7 +8,7 @@ function PhotoSection() {
             <img src="./homeimg/photographer.jpg" alt="photographer" />
             <div className="home__details">
                 <h2>Cameraman & Photographer</h2>
-                <h4>Group, Event, Occasions</h4>
+                <h4> Book for any event!</h4>
                 <p>Photographers use a variety of photographic equipment to capture events, people, objects or scenes for records, art or commercial use. Many photographers begin their careers as photographic assistants, setting up lights and stands, arranging transport and occasionally acting as a 'second shooter'.</p>
                 <Link className="button__details" to="/photo">
                     <button>Learn More</button></Link>
